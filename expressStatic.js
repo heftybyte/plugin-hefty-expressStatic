b130201968e14c88338f6b6dc1b2159cd17cd12d
Esco Obong <menzoic@gmail.com>
2013-07-15 04:03:13 -0400

    initial commit

---
+++ expressStatic.js
@@ -0,0 +1,19 @@
+module.exports = function setup(options imports, register) {
+
+	var httpServer = imports.httpServer;
+	var express = require('express');
+	var consolidate = require('consolidate')
+	var swig = require('swig');
+	var path = require('path');
+
+	httpServer.engine('.html', consolidate.swig);
+	httpServer.set('view engine', 'html');
+
+	swig.init({
+		root: options.viewDir,
+		allowErrors: options.allowErrors || false
+	});
+
+	httpServer.set('views', options.viewDir);
+	httpServer.use(express.static(path.join(__dirname, options.publicDir)));
+}
